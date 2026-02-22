65accbe512b6203b61817303da871aa26e9ec41b
Azraei Yusof <mazei513@gmail.com>
2026-02-22 20:14:29 +0800

    add API_URL var
    17 to 18 lines, trash programmer

--- myweather.fish
+++ myweather.fish
@@ -1,9 +1,10 @@
 function myweather
+    set API_URL "https://api.data.gov.my/weather/forecast"
     set LOC_ID_FILE ~/.config/myweather/loc_id
     set LOC_ID (cat $LOC_ID_FILE)
     if test -z $LOC_ID
         echo "Set location ID at $LOC_ID_FILE, do the following to get locations"
-        echo "  curl -s -L 'https://api.data.gov.my/weather/forecast' | jq '[.[].location | {\"key\": .location_id, \"value\": .location_name}] | from_entries'"
+        echo "  curl -s -L '$API_URL' | jq '[.[].location | {\"key\": .location_id, \"value\": .location_name}] | from_entries'"
         return 1
     end
 
@@ -11,7 +12,7 @@ function myweather
     mkdir -p $CACHE_PATH
     set CACHE_FILE "$CACHE_PATH/cached.json"
     if not test -e $CACHE_FILE || test (math (date -d 0 +%s) - (path mtime $CACHE_FILE)) -ge 0
-        curl -s -L "https://api.data.gov.my/weather/forecast?contains=$LOC_ID@location__location_id" >$CACHE_FILE
+        curl -s -L "$API_URL?contains=$LOC_ID@location__location_id" >$CACHE_FILE
     end
     jq -r '.[] | [.date, .morning_forecast, .afternoon_forecast, .night_forecast, .min_temp, .max_temp] | @csv' $CACHE_FILE | sed 's/Tiada hujan/☀️/g' | sed 's/Hujan/🌧️/g' | sed 's/Ribut petir/⛈️/g' | sed 's/"//g' | sed 's/,/ /g'
 end
